79e3cb19c8c1439b7f99c8433daee424c8798a35
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-02-28 14:02:58 +0530

    Create GCD.cpp

---
+++ Basic Concepts/GCD.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    int count = 0;
+    for(int i = 1; i*i<=n; i++){
+        if(n%i==0){
+            count++;
+            if((n/i) != i) count++;
+        }
+    }
+    if(count == 2) cout << "true";
+        else cout << "false";
+    
+    return 0;
+}
