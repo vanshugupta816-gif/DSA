feb048bd8c1824e22a3ec9f0ef258bc044b46895
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-08 21:22:53 +0530

    Create QuickSort.cpp

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int partition (vector<int>& arr, int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+        while (arr [j] > pivot && j >= low + 1) {
+            j--;
+        }
+        if (i < j) swap (arr[i], arr[j]);
+     }
+     swap (arr [low], arr[j]);
+     return j;
+}
+void qs(vector <int> & arr, int low, int high){
+    if(low < high){
+        int pIndex = partition (arr, low, high);
+        qs(arr, low, pIndex-1);
+        qs(arr, pIndex+1, high);
+    }
+}
+
+int main(){
+     vector<int> arr = {4, 1, 3, 9, 7};
+     qs(arr, 0, arr.size() - 1);
+        for (int x : arr)
+        cout << x << " ";
+       
+    
+    return 0;
+}
