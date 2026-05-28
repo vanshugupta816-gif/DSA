353aceef3a66907aa49bffcf112a5820e7ff74fc
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-05-28 18:46:31 +0530

    Create nth root of an integer.cpp

---
+++ Binary Search/nth root of an integer.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+double multiply(double number, int n)
+{
+    double ans = 1.0;
+    for (int i = 1; i <= n; i++)
+    {
+        ans = ans * number;
+    }
+    return ans;
+}
+double getNthRoot(int n, int m)
+{
+    double low = 1;
+    double high = m;
+    double eps = 1e-6;
+    while ((high - low) > eps)
+    {
+        double mid = (low + high) / 2.0;
+        if (multiply(mid, n) < m)
+        {
+            low = mid;
+        }
+        else
+        {
+            high = mid;
+        }
+    }
+    cout << low << " " << high << endl;
+    cout << pow(m, (double)(1.0 / (double)n));
+    return low;
+}
+int main()
+{
+    int n = 3;
+    int m = 27;
+    getNthRoot(n, m);
+    return 0;
+}
