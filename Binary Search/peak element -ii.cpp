7baa0f97494573a363cdcb7145c7c3ccbd73260c
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-08-01 21:08:13 +0530

    Create peak element -ii.cpp

---
+++ Binary Search/peak element -ii.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findMaxIndex(vector<vector<int>> &mat, int n, int m, int col)
+{
+    int maxValue = -1;
+    int index = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (mat[i][col] > maxValue)
+        {
+            maxValue = mat[i][col];
+            index = i;
+        }
+    }
+    return index;
+}
+vector<int> findPeakGrid(vector<vector<int>> &mat)
+{
+    int n = mat.size();
+    int m = mat[0].size();
+    int low = 0, high = m - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int maxRowIndex = findMaxIndex(mat, n, m, mid);
+        int left = (mid - 1 >= 0) ? mat[maxRowIndex][mid - 1] : -1;
+        int right = (mid + 1 < m) ? mat[maxRowIndex][mid + 1] : -1;
+        if (mat[maxRowIndex][mid] > left &&
+            mat[maxRowIndex][mid] > right)
+        {
+            return {maxRowIndex, mid};
+        }
+        else if (mat[maxRowIndex][mid] < left)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return {-1, -1};
+}
+
+int main()
+{
+    vector<vector<int>> mat = {
+        {10, 20, 15},
+        {21, 30, 14},
+        {7, 16, 32}};
+    vector<int> ans = findPeakGrid(mat);
+    cout << "Peak Element Position: (" << ans[0] << ", " << ans[1] << ")" << endl;
+    cout << "Peak Element Value: " << mat[ans[0]][ans[1]] << endl;
+    return 0;
+}
