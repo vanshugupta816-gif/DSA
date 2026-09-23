33a72a198bf95f40fe5b0d772e2a5bbf4c71ca52
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-09-23 15:40:04 +0530

    Create find middle element.cpp

---
+++ Linked List/Medium of LL/find middle element.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+Node *findMiddle(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+int main()
+{
+    // Creating linked list:
+    // 1 -> 2 -> 3 -> 4 -> 5
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    Node *middle = findMiddle(head);
+    cout << "Middle element: " << middle->data << endl;
+    return 0;
+}
